637f62c108b73b5e9c8c49b57af2b4ca41d2940b
wdhmonk <wesley.hucker@mediamonks.com>
2023-08-29 23:33:32 +1000

    ok??

--- src/components/guessing.tsx
+++ src/components/guessing.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Input } from "@/components/ui/input"
+import { Input } from "~/components/input"
 
 
 const movieId = Math.floor(Math.random() * 1000);
