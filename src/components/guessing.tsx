b511131f6e697a050d0acba3399f1b0f1cc83947
wdhmonk <wesley.hucker@mediamonks.com>
2023-08-30 13:28:10 +1000

    center aligned text

--- src/components/guessing.tsx
+++ src/components/guessing.tsx
@@ -66,7 +66,7 @@ export default function Guessing() {
   return (
     <>
       <img src={imageUrl} alt="" />
-      <h1 className="text-white font-bold m-10 text-5xl">Guess the movie</h1>
+      <h1 className="text-white font-bold m-10 text-5xl text-center">Guess the movie</h1>
       <h2 className="text-white opacity-10 m-10">Hint: {title}</h2>
       <InputDemo correctAnswer={title} onNextMovie={handleNextMovie} />
       <Button variant="destructive" className = "p-10 w-64"onClick={handleNextMovie}>I give up</Button>
